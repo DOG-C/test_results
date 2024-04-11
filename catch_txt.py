e8163cd722995981e373d1b9011aa66f322ddadc
caoyuqi1996@hotmail.com <caoyuqi1996@hotmail.com>
2024-04-11 20:58:39 +0200

    one file implemented

--- catch_txt.py
+++ catch_txt.py
@@ -6,17 +6,22 @@ def capture_text_from_end(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
     
-    pattern = r'(test execution summary.*)'
+    pattern = r'(test execution summary.*?)(?=(test execution summary|$))'
     matches = re.findall(pattern, text, re.DOTALL)
     
-    return matches[-1] if matches else ""
+    if matches:
+        # 获取最后一个匹配项的内容
+        last_match = matches[-1][0]  # 修改这里以获取正确的匹配组
+        return last_match
+    else:
+        return "未找到指定内容"
 
-def text_to_image(text, output_path, font_path='C:\\Windows\\Fonts\\Consola.ttf', font_size=10):
+def text_to_image(text, output_path, font_path='C:\\Windows\\Fonts\\Consola.ttf', font_size=12):
     # 根据文本长度和字体大小估算所需的图像尺寸
     lines = text.split('\n')
     max_line_length = max(len(line) for line in lines)
     image_width = max_line_length * font_size
-    image_height = len(lines) * font_size * 1.5  # 为行间距留出空间
+    image_height = len(lines) * font_size * 1  # 为行间距留出空间
     
     image = Image.new('RGB', (int(image_width), int(image_height)), color = (0, 0, 0))
     draw = ImageDraw.Draw(image)
@@ -25,13 +30,13 @@ def text_to_image(text, output_path, font_path='C:\\Windows\\Fonts\\Consola.ttf'
     y_text = 0
     for line in lines:
         draw.text((0, y_text), line, font=font, fill=(255, 255, 255))
-        y_text += font_size * 1.5  # 更新下一行的y坐标
+        y_text += font_size * 1  # 更新下一行的y坐标
     
     # 确保输出目录存在
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     image.save(output_path)
 
-file_path = 'your_file_path.txt'
+file_path = os.path.join('results', 'April_11', 'OAK-170_esu_drv.txt')
 captured_text = capture_text_from_end(file_path)
 
 # 构造输出图片的路径
